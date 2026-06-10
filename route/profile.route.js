930d50ed14f77c5e9131176d0abd56c5a74ee9d4
arbajsiprogrammer <arbajsiprogrammer@gmail.com>
2026-06-10 11:28:07 +0530

    add profile route with authentication for user actions

---
+++ route/profile.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { verifyAuthToken } from "../middleware/auth.middleware.js";
+import { deleteUser, logout, profile } from "../controller/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/logout", verifyAuthToken, logout);
+
+router.delete("/delete", verifyAuthToken, deleteUser);
+
+router.get("/", verifyAuthToken, profile);
+
+export default router;
